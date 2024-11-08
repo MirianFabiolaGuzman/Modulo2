04eb7aa5deec1aa69e5329a9f4c3ee9420c00cb9
MirianFabiolaGuzman <fabiolaguzmanmoreno25@gmail.com>
2024-11-08 16:22:21 -0600

    Ejercicio2

---
+++ Persona.java
@@ -0,0 +1,13 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nuevoNombre) {
+        nombre = nuevoNombre;
+    }
+    
+}
